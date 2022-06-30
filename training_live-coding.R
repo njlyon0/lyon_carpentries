5ff9c50dbf13bf9de1edbf382cea12440c3a693e
njlyon0 <nicholasjlyon@gmail.com>
2022-06-30 13:54:14 -0500

    Pre-structured the package call and data acquisition

---
+++ training_live-coding.R
@@ -0,0 +1,10 @@
+# Load needed libraries
+install.packages("tidyverse")
+library(tidyverse)
+
+# Can use pre-loaded data called "iris"
+## `head()` returns first 6 rows of all columns
+head(iris)
+
+# Tidyverse content!
+
